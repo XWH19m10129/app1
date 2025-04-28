1012134af722ad3cff5d6401fd7307f18ce79e24
xuewh22 <xuewh22@mails.tsinghua.edu.cn>
2025-04-28 14:15:18 +0800

    add database support

--- app.py
+++ app.py
@@ -1,7 +1,58 @@
 from flask import Flask, render_template, request, redirect, url_for, session, flash
 from markupsafe import escape
-from flask import url_for
+from flask_sqlalchemy import SQLAlchemy
+import os
+import sys
+import click
+
+
+WIN = sys.platform.startswith('win')
+if WIN:  # 如果是 Windows 系统，使用三个斜线
+    prefix = 'sqlite:///'
+else:  # 否则使用四个斜线
+    prefix = 'sqlite:////'
+# app = Flask(__name__)
+# db = SQLAlchemy(app) 
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(app.root_path, 'data.db')
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(app.root_path, 'data.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # 关闭对模型修改的监控
+# 在扩展类实例化前加载配置
+db = SQLAlchemy(app)
+@app.cli.command()
+def forge():
+    """Generate fake data."""
+    db.create_all()
+
+    # 全局的两个变量移动到这个函数内
+    name = 'xuewh22'
+    movies = [
+        {'title': 'Frozen', 'year': '2013'},
+        {'title': 'Zootopia', 'year': '2016'},
+        {'title': 'starwars', 'year': '2015'},
+        {'title': 'Encanto', 'year': '2021'},
+        {'title': 'your name', 'year': '2016'},
+        {'title': 'weathering with you', 'year': '2019'},
+        {'title': 'x-men:dark phoenix', 'year': '2019'},
+    ]
+
+    user = User(name=name)
+    db.session.add(user)
+    for m in movies:
+        movie = Movie(title=m['title'], year=m['year'])
+        db.session.add(movie)
+
+    db.session.commit()
+    click.echo('Done.')
+
+@app.cli.command()  # 注册为命令，可以传入 name 参数来自定义命令
+@click.option('--drop', is_flag=True, help='Create after drop.')  # 设置选项
+def initdb(drop):
+    """Initialize the database."""
+    if drop:  # 判断是否输入了选项
+        db.drop_all()
+    db.create_all()
+    click.echo('Initialized database.')  # 输出提示信息
 
 @app.route('/user/<name>')
 def user_page(name):
@@ -9,7 +60,9 @@ def user_page(name):
 
 @app.route('/')
 def index():
-    return render_template('index.html', name=name, movies=movies)
+    user = User.query.first()  # 读取用户记录
+    movies = Movie.query.all()  # 读取所有电影记录
+    return render_template('index.html', user=user, movies=movies)
 @app.route('/skating')
 def skating():
     return 'welcome to skating part!'
@@ -40,6 +93,14 @@ movies = [
     {'title': 'The Pork of Music', 'year': '2012'},
 ]
 
+class User(db.Model):  # 表名将会是 user（自动生成，小写处理）
+    id = db.Column(db.Integer, primary_key=True)  # 主键
+    name = db.Column(db.String(20))  # 名字
 
+
+class Movie(db.Model):  # 表名将会是 movie
+    id = db.Column(db.Integer, primary_key=True)  # 主键
+    title = db.Column(db.String(60))  # 电影标题
+    year = db.Column(db.String(4))  # 电影年份
 # if __name__ == '__main__':
     # app.run(debug=True)
