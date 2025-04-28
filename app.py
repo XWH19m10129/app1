e27129a37780189bf5b3cfaf5e80872ae8ef1782
xuewh22 <xuewh22@mails.tsinghua.edu.cn>
2025-04-28 14:44:49 +0800

    add base template and context processor

--- app.py
+++ app.py
@@ -54,30 +54,41 @@ def initdb(drop):
     db.create_all()
     click.echo('Initialized database.')  # 输出提示信息
 
-@app.route('/user/<name>')
-def user_page(name):
-    return f'User: {escape(name)}'
+# @app.route('/user/<name>')
+# def user_page(name):
+#     return f'User: {escape(name)}'
 
 @app.route('/')
 def index():
-    user = User.query.first()  # 读取用户记录
+    # user = User.query.first()  # 读取用户记录
     movies = Movie.query.all()  # 读取所有电影记录
-    return render_template('index.html', user=user, movies=movies)
-@app.route('/skating')
-def skating():
-    return 'welcome to skating part!'
+    return render_template('index.html', movies=movies)
 
-@app.route('/test')
-def test_url_for():
-    # 下面是一些调用示例（请访问 http://localhost:5000/test 后在命令行窗口查看输出的 URL）：
-    # print(url_for('hello'))  # 生成 hello 视图函数对应的 URL，将会输出：/
-    # 注意下面两个调用是如何生成包含 URL 变量的 URL 的
-    print(url_for('user_page', name='greyli'))  # 输出：/user/greyli
-    print(url_for('user_page', name='peter'))  # 输出：/user/peter
-    print(url_for('test_url_for'))  # 输出：/test
-    # 下面这个调用传入了多余的关键字参数，它们会被作为查询字符串附加到 URL 后面。
-    print(url_for('test_url_for', num=2))  # 输出：/test?num=2
-    return 'Test page'
+# @app.route('/skating')
+# def skating():
+#     return 'welcome to skating part!'
+
+# @app.route('/test')
+# def test_url_for():
+#     # 下面是一些调用示例（请访问 http://localhost:5000/test 后在命令行窗口查看输出的 URL）：
+#     # print(url_for('hello'))  # 生成 hello 视图函数对应的 URL，将会输出：/
+#     # 注意下面两个调用是如何生成包含 URL 变量的 URL 的
+#     print(url_for('user_page', name='greyli'))  # 输出：/user/greyli
+#     print(url_for('user_page', name='peter'))  # 输出：/user/peter
+#     print(url_for('test_url_for'))  # 输出：/test
+#     # 下面这个调用传入了多余的关键字参数，它们会被作为查询字符串附加到 URL 后面。
+#     print(url_for('test_url_for', num=2))  # 输出：/test?num=2
+#     return 'Test page'
+
+@app.errorhandler(404)  # 传入要处理的错误代码
+def page_not_found(e):  # 接受异常对象作为参数
+    user = User.query.first()
+    return render_template('404.html'), 404  # 返回模板和状态码
+
+@app.context_processor
+def inject_user():
+    user = User.query.first()
+    return dict(user=user)  # 返回一个字典，模板中可以直接使用 user 变量
 
 name = 'xuewh22'
 movies = [
